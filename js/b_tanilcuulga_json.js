cbdcfc2c29133882c2c6ebd0ebd805bcb3f91861
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-11-10 23:40:32 +0800

    dalai

--- js/b_tanilcuulga_json.js
+++ js/b_tanilcuulga_json.js
@@ -1,64 +1,246 @@
 const sublist=[
     {
         "content":"МӨНХ ТУНХ",
-        "link":"#section1",
-        "label":"Мөнх тунх бүтээгдэхүүний мэдээлэл"},
+        "link":"section1",
+        "label":"Мөнх тунх бүтээгдэхүүний мэдээлэл",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/м.jpg",
+                "name":"MUNH",
+                "descrip":"Анх хараад л идэх хүсэл төрүүлнэ шоколад, цөцгийтэй MUNH"
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/tus_tunh.jpg",
+                "name":"TUNH",
+                "descrip":"Солонго шиг амттай гүзээлзгэнэ, цөцгийтэй сахарын агууламжгүй TUNH"
+            }
+        ]
+    },
     {
         "content":"ЖИМСТЭЙ ЗАЙРМАГ",
-        "link":"#section1",
-        "label":"Жимстэй зайрмагнуудын мэдээлэл"},
+        "link":"section2",
+        "label":"Жимстэй зайрмагнуудын мэдээлэл",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/iberry.jpg",
+                "name":"IBERRY",
+                "descrip":"Инээмсэглэл бэлэглэх амт, байгалийн зэрлэг жимс IBERRY."
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/mello1.jpg",
+                "name":"MELLO",
+                "descrip":"MELLO киви, гүзээлзгэнэ, черриний дур булаам амт."
+            }
+        ]
+    },
     {
         "content":"ШОКОЛАДТАЙ ЗАЙРМАГ",
-        "link":"#section1",
-        "label":"Шоколадтай зайрмагнуудын мэдээлэл"},
+        "link":"section3",
+        "label":"Шоколадтай зайрмагнуудын мэдээлэл",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/IMG_8548.jpg",
+                "name":"VENUS",
+                "descrip":"Хайр дурлалын амт VENUS"
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/3d,4d_icemarkzairmag.png",
+                "name":"MELLO",
+                "descrip":"Төсөөллөөс ч илүү гүзээлзгэнэ, шоколадтай зайрмаг ICEMARK"
+            }
+        ]
+    },
     {
         "content":"MOCKTAIL",
-        "link":"#section1",
-        "label":"Mocktail брендийн зайрмагнуудын мэдээлэл"},
+        "link":"section4",
+        "label":"Mocktail брендийн зайрмагнуудын мэдээлэл",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/mocktai;.jpg",
+                "name":"MOCKTAIL",
+                "descrip":"Зөвхөн чамтай л энэ төгс амтыг хуваалцмаар MOCKTAIL"
+            }
+        ]
+        
+    },
     {
         "content":"ААРЦТАЙ ЗАЙРМАГ",
-        "link":"#section1",
-        "label":"аарцтай зайрмагнуудын мэдээлэл"},
+        "link":"section5",
+        "label":"аарцтай зайрмагнуудын мэдээлэл",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/бамбар.jpg",
+                "name":"BAMBAR",
+                "descrip":"Дархлаа дэмжигч аарцтай, чацарганатай, гүзээлзгэнэтэй BAMBAR"
+            }
+        ]
+    },
     {
         "content":"АЯГАТАЙ ЗАЙРМАГ",
-        "link":"#section1",
-        "label":"Аягатай зайрмагнуудын танилцуулга"},
+        "link":"section6",
+        "label":"Аягатай зайрмагнуудын танилцуулга",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/teso_ice.jpg",
+                "name":"TESO",
+                "descrip":"Иристэй цөцгий аягатай TESO"
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/элитэ.jpg",
+                "name":"ELITE",
+                "descrip":"Дур булаам амт, аягатай ELITE."
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/mocha.jpg",
+                "name":"MOCHA",
+                "descrip":"Ногоон цайтай зайрмаг таныг таргалуулахгүй"
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/иогурт.jpg",
+                "name":"YOGURT ICECREAM",
+                "descrip":"Эрүүл мэндийн зайрмаг YOGURT ICECREAM"
+            }
+        ]
+    
+    },
     {
         "content":"ЦӨЦГИЙТЭЙ ЗАЙРМАГ",
-        "link":"#section1",
-        "label":"Цөцгийтэй зайрмагнуудын танилцуулга"},
+        "link":"section7",
+        "label":"Цөцгийтэй зайрмагнуудын танилцуулга",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/alyaskaface.png",
+                "name":"ALYASKA",
+                "descrip":"Амтат цөцгий эрүүл амттан ALYASKA"
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/standartface.jpg",
+                "name":"STANDART",
+                "descrip":"Уруул дурлам аз жаргалын амт STANDART"
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/тус_.jpg",
+                "name":"PLOMBIR",
+                "descrip":"Цөцгий, самар, ирсисний төгс хослол PLOMBIR"
+            }
+        ]
+    
+    },
     {
         "content":"ГЭР БҮЛИЙН",
-        "link":"#section1",
-        "label":"гэр бүлийн зайрмагнуудын танилцуулга"}
+        "link":"section8",
+        "label":"гэр бүлийн зайрмагнуудын танилцуулга",
+        "icecream":[
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/империа.jpg",
+                "name":"IMPERIA CAKE ICECREAM",
+                "descrip":"Хайртдаа өгөх хамгийн гоё бэлэг IMPERIA зайрмаган бялуу."
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/IMG_8549.jpg",
+                "name":"MINI SHOCK",
+                "descrip":"Аз жаргалын амтаа хамтдаа хуваалцах хамгийн шилдэг амттан бол MINI SHOCK"
+            },
+            {
+                "img":"pictures_bvteegdehvvniitanilcuulga/elite ice.jpg",
+                "name":"ELITE",
+                "descrip":"Гэр бүлээрээ идэхэд хамгийн тохиромжтой ELITE"
+            }
+        ]
+    }
 ];
 
 class MySubList{
-    constructor(content,link,label) {
+    constructor(content,link,label,icecream) {
         this.content = content;
         this.link = link;
         this.label = label;
+        this.icecream=icecream;
     }
-
-    render() {
-        return `<li><a href="${this.link}" aria-label="${this.label}">${this.content}</a>
+    render_menu() {
+        return `<li><a href="#${this.link}" aria-label="${this.label}">${this.content}</a>
         <div class="line"></div>
         </li>`;
     }
+    render_tanilcuulga(){
+        if(this.content!="MOCKTAIL"){
+            let cont="";
+            for(const ics of this.icecream){
+                cont+=`<article class="zairmag">
+                <a href="#" aria-label="${ics.name} зайрмагны дэлгэрэнгүй мэдээлэл" onclick="${ics.name}()">
+                <div class="zurag">
+                    <img src="${ics.img}" alt="${ics.name} зайрмаг">
+                    <div>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                </div>
+                <h1>${ics.name}</h1>
+                <div class="bvteegdehvvn_tailbar">
+                    <span>${ics.descrip}</span>
+                    <div><svg viewBox="0 0 137.83 38.81" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+                            class="c-icon-text-link__icon">
+                            <path
+                                d="M137.1 17.64L120.2.73a2.5 2.5 0 00-3.54 3.54l12.15 12.14c-15.28-2.12-21 1.19-25.63 3.88-2.73 1.58-4.88 2.82-8.78 2.82-3.18 0-4.87-2-7.38-5.23s-5.53-7.19-11.34-7.19-8.79 4-11.16 7.23-4 5.19-7.13 5.19-4.77-2-7.14-5.19-5.32-7.23-11.16-7.23-8.79 4-11.16 7.23-4 5.19-7.13 5.19-4.77-2-7.14-5.19-5.32-7.23-11.16-7.23a2.5 2.5 0 000 5c3.15 0 4.76 2 7.13 5.2S15 28.11 20.8 28.11s8.79-4 11.16-7.22 4-5.2 7.13-5.2 4.77 2 7.14 5.2 5.32 7.22 11.16 7.22 8.79-4 11.16-7.22 4-5.2 7.13-5.2 4.88 2 7.38 5.24 5.54 7.18 11.34 7.18c5.24 0 8.31-1.78 11.28-3.5 4.33-2.5 9.22-5.33 23.94-3l-13 13a2.5 2.5 0 003.54 3.54l16.9-16.91a2.49 2.49 0 00.04-3.6z">
+                            </path>
+                        </svg><em>дэлгэрэнгүй</em></div>
+                </div>
+                </a>
+                </article>`;
+            }
+            return`<section id="${this.link}" class="turul">
+            <h1>${this.content}</h1>
+            </section>
+            <section class="tanilcuulga_icecream">`+cont+`</section>`;
+        }
+        else{
+            let cont="";
+            for(const ics of this.icecream){
+                cont+=`<article class="zairmag">
+                <a href="#" aria-label="${ics.name} зайрмагны дэлгэрэнгүй мэдээлэл" onclick="${ics.name}()">
+                <div class="zurag">
+                    <img src="${ics.img}" alt="${ics.name} зайрмаг">
+                    <div>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                </div>
+                <h1 id="Mocktail_h1">${ics.name}</h1>
+                <div class="bvteegdehvvn_tailbar" id="mocktail_foot">
+                    <span>${ics.descrip}</span>
+                    <div><svg viewBox="0 0 137.83 38.81" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+                            class="c-icon-text-link__icon">
+                            <path
+                                d="M137.1 17.64L120.2.73a2.5 2.5 0 00-3.54 3.54l12.15 12.14c-15.28-2.12-21 1.19-25.63 3.88-2.73 1.58-4.88 2.82-8.78 2.82-3.18 0-4.87-2-7.38-5.23s-5.53-7.19-11.34-7.19-8.79 4-11.16 7.23-4 5.19-7.13 5.19-4.77-2-7.14-5.19-5.32-7.23-11.16-7.23-8.79 4-11.16 7.23-4 5.19-7.13 5.19-4.77-2-7.14-5.19-5.32-7.23-11.16-7.23a2.5 2.5 0 000 5c3.15 0 4.76 2 7.13 5.2S15 28.11 20.8 28.11s8.79-4 11.16-7.22 4-5.2 7.13-5.2 4.77 2 7.14 5.2 5.32 7.22 11.16 7.22 8.79-4 11.16-7.22 4-5.2 7.13-5.2 4.88 2 7.38 5.24 5.54 7.18 11.34 7.18c5.24 0 8.31-1.78 11.28-3.5 4.33-2.5 9.22-5.33 23.94-3l-13 13a2.5 2.5 0 003.54 3.54l16.9-16.91a2.49 2.49 0 00.04-3.6z">
+                            </path>
+                        </svg><em>дэлгэрэнгүй</em></div>
+                </div>
+                </a>
+                </article>`;
+            }
+            return`<section id="${this.link}" class="turul">
+            <h1>${this.content}</h1>
+            </section>
+            <section class="tanilcuulga_icecream" id="mocktail_page">`+cont+`
+            </section>`;
+        }
+    }
 }
-App(sublist);
+
 function App(SList) {
-    
     let retVal = "<ul>";
-    for (const key in SList) {
+    let retVal_tanilcuulga="";
+    for (const key of SList){
         console.log(key.content);
-        let articleObj = new MySubList(key.content,key.link,key.label);
-        retVal += articleObj.render();
+        let articleObj = new MySubList(key.content,key.link,key.label,key.icecream);
+        retVal += articleObj.render_menu();
+        retVal_tanilcuulga+=articleObj.render_tanilcuulga();
     }
     retVal+="</ul>";
-    document.querySelector(".bvteegdehvvnii_turul").innerHTML=retVal;
+    document.getElementsByClassName("bvteegdehvvnii_turul")[0].innerHTML=retVal;
+    document.querySelector(".bvteegdehvvnii_turul").insertAdjacentHTML("afterend",retVal_tanilcuulga);
 }
-
-
-
+window.onload=()=>App(sublist);
 
