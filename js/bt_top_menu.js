a07801e90e2e85cc091e21ec98131cd4c171edf1
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-11-14 23:36:24 +0800

    da

--- js/bt_top_menu.js
+++ js/bt_top_menu.js
@@ -1,20 +1,16 @@
-
-
 resize_top_menu();
+let mql = window.matchMedia('(min-width: 691px)');
 let scroll_pos;
 window.onscroll=()=>{scroll()};
 function scroll() {
-    let elm=document.querySelector(".bvteegdehvvnii_turul");
-    if (document.documentElement.scrollTop > scroll_pos) {
-        if(window.screen.width>691){
-            elm.style.display="block";
-        }
+let elm=document.querySelector(".bvteegdehvvnii_turul");
+    if (document.documentElement.scrollTop > scroll_pos && mql.matches) {
+        elm.style.display="block";   
     } else {
         elm.style.display="none";
     }
     scroll_pos=document.documentElement.scrollTop;
 }
-
 function resize_top_menu(){
     const elmnt = document.querySelector("header");
     let height=elmnt.offsetHeight;
--- js/open_window.js
+++ js/open_window.js
@@ -1,71 +1,76 @@
-
-
-
+let tap;
 function MUNH(){
-    document.getElementById("MUNH").style.display="none";
+    document.getElementById("MUNH").style.display="block";
 }
            
 function TUNH(){
-    document.getElementById("TUNH").style.display="none";
+    document.getElementById("TUNH").style.display="block";
 }
 
 function MELLO(){
-    document.getElementById("MELLO").style.display="none";
+    tap=document.getElementById("MELLO");
+    tap.parentNode.style.display="block";
+    tap.parentNode.addEventListener('click',()=>{
+            tap.parentNode.style.display="none";
+    });
 }
-       
 function VENUS(){
-    document.getElementById("VENUS").style.display="none";
+    document.getElementById("VENUS").parentNode.style.display="block";
 }
 function IBERRY(){
-    document.getElementById("IBERRY").style.display="none";
+    tap=document.getElementById("IBERRY");
+    tap.parentNode.style.display="block";
+    tap.childNodes[1].childNodes[1].addEventListener('click',()=>{
+            tap.parentNode.style.display="none";
+    });
 }
 function MOCKTAIL(){
-    document.getElementById("mocktail_window").style.display="none";
+    document.getElementById("mocktail_window").style.display="block";
 }
 
 function BAMBAR(){
-    document.getElementById("BAMBAR").style.display="none";
+    document.getElementById("BAMBAR").style.display="block";
 }
                
 function TESO(){
-    document.getElementById("TESO").style.display="none";
+    document.getElementById("TESO").style.display="block";
 }
            
 function ELITE(){
-    document.getElementById("ELITE").style.display="none";
+    document.getElementById("ELITE").style.display="block";
 }
          
 function MOCHA(){
-    document.getElementById("MOCHA").style.display="none";
+    document.getElementById("MOCHA").style.display="block";
 }
              
 function YOGURT(){
-    document.getElementById("YOGURT").style.display="none";
+    document.getElementById("YOGURT").style.display="block";
 }
               
 function ALYASKA(){
-    document.getElementById("ALYASKA").style.display="none";
+    document.getElementById("ALYASKA").style.display="block";
 }
        
 function STANDART(){
-    document.getElementById("STANDART").style.display="none";
+    document.getElementById("STANDART").style.display="block";
 }
               
 function PLOMBIR(){
-    document.getElementById("PLOMBIR").style.display="none";
+    document.getElementById("PLOMBIR").style.display="block";
 }
           
 function IMPERIA(){
-    document.getElementById("IMPERIA").style.display="none";
+    document.getElementById("IMPERIA").style.display="block";
 }
              
 function MINI(){
-    document.getElementById("MINI_SHCOK").style.display="none";
+    document.getElementById("MINI_SHCOK").style.display="block";
 
 }
           
 function ELITE(){
-    document.getElementById("ELITE").style.display="none";
+    document.getElementById("ELITE").style.display="block";
 }
            
 function ICEMARK(){
@@ -74,7 +79,3 @@ function ICEMARK(){
 
 
 
-var x=document.getElementById("x");
-x.addEventListener("click",()=>{
-    x.parentNode.parentNode.style.display="none";
-});
