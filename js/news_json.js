0bb2801962f1806a7f78f96268f8a354a166a4a8
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-11-25 15:15:05 +0800

    Update news_json.js

--- js/news_json.js
+++ js/news_json.js
@@ -1,3 +1,3 @@
 import defaultExport from "../modules/medee_medeelel.js";
-const recentNews = new defaultExport("../js/medee.json");
+const recentNews = new defaultExport("./medee.json");
 recentNews.download();
