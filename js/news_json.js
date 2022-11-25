778c0fae5eb2fcaba6a87570bdbd1c7b3dc1b73f
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-11-25 14:53:14 +0800

    paths

--- js/news_json.js
+++ js/news_json.js
@@ -1,3 +1,3 @@
 import defaultExport from "../modules/medee_medeelel.js";
-const recentNews = new defaultExport("https://api.jsonbin.io/v3/b/6378dd622b3499323b04585d");
+const recentNews = new defaultExport("../json/medee.json");
 recentNews.download();
