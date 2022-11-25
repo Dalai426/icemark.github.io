a31e5a73ae596654fa2becb1fe9f2c071c225e75
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-11-25 09:40:13 +0800

    Update medee_medeelel.js

--- modules/medee_medeelel.js
+++ modules/medee_medeelel.js
@@ -1,4 +1,5 @@
 class RecentNewsItem { 
+    //constructor
     constructor(news) {
         this.src = news.src;
         this.mmin = news.min;
@@ -8,6 +9,7 @@ class RecentNewsItem {
         this.content=news.content;
         this.date=news.date;
     }
+    //build and return html
     Render() {
         return ` <section class="news"><a href="${this.href}">
         <picture>
@@ -35,17 +37,13 @@ export default class RecentNews{
     }
     
     download(){
-        fetch(`${this.jsonUrl}`,{
-        method:'POST',
-        headers:{
-            'Accept':'application/json',
-            'Content-Type':'application/json',
-        },
-        mode:'no-cors'})
+        fetch(`${this.jsonUrl}`,
+        { mode: "no-cors" })
         .then(response=>{
             response.json()
             .then(jsob_data=>{
                 document.getElementsByTagName("main")[0].innerHTML=``;
+                this.pages(jsob_data.length);
                 const filteredArray=jsob_data.filter((news,index)=>{return this.current_page*10<=index && index<this.current_page*10+10});
                 document.getElementsByTagName("main")[0].insertAdjacentHTML("afterbegin",
                     filteredArray.map(newNews=>{
@@ -60,5 +58,124 @@ export default class RecentNews{
         .catch(err => { console.log(err); });
     }
     
+    pages(data_len){
+        data_len=Math.ceil(data_len/10);
+        this.last_page=data_len-1;
+        data_len*=10;
+        let ret=`<a href="#" id="prv"> Previous </a>`
+            let start=parseInt(this.current_page/3)*3;
+            data_len=parseInt((data_len-(start+1)*10)/10);
+            if(3<data_len){
+                data_len=3;
+            }
+            for (let i=start;i<=start+data_len;i++) {
+                ret+=`<a href="#" class="page"> ${i+1} </a>`;
+            }
+        ret+=`<a href="#" id="nex"> Next </a>`;
+        let contain=document.querySelector(".link");
+        contain.innerHTML=``;
+        contain.innerHTML+=ret;
+        document.getElementById("nex").addEventListener("click",()=>(this.page_p(1)));
+        document.getElementById("prv").addEventListener("click",()=>(this.page_p(-1)));
+        for (let i=start;i<=start+data_len;i++) {
+            document.getElementsByClassName("page")[i-start].addEventListener("click",()=>{this.change_page(i)});
+        }
+    } 
+    page_p(page){
+        if(this.current_page==0 && page==-1){
+        }
+        else if(this.current_page==this.last_page && page==1){
+        }
+        else{
+            this.current_page+=page;
+            this.download();
+        }
+    }
+    change_page(pno){
+        this.current_page=pno;
+        this.download();
+    }
 }
 
+
+/*download(){
+        this.jsobs=fetch(`${this.jsonUrl}`)
+        .then(response=>{
+            response.json()
+            .then(
+                jsob_data=>{
+                            console.log(jsob_data);
+                            jsob_data.map(newNews=>{
+                                const articleObj = new RecentNewsItem(newNews);
+                                this.recentNewsList.push(articleObj);});
+                                console.log(this.recentNewsList.length);
+                }
+                
+            )
+        })
+        .catch(err => { console.log(err) });
+    }
+    render(){
+        console.log(this.recentNewsList.length);
+ 
+            document.getElementsByTagName("main")[0].insertAdjacentHTML("afterbegin",
+                this.recentNewsList.map(newNews=>{
+                    const articleObj = new RecentNewsItem(newNews);
+                    return articleObj.Render();
+                })
+                .reduce((pre, cur) => pre+cur, "")
+            );
+        
+ 
+    }
+*/
+
+/*
+constructor(jsonUrl) {
+        this.recentNewsList = [];
+        this.jsonUrl = jsonUrl;
+        this.jsobs;
+        this.current_page=4;
+    }
+    download(){
+        this.jsobs=fetch(`${this.jsonUrl}`)
+        .then(response=>{
+            response.json()
+            .then(jsob_data=>{
+                this.len=jsob_data.length;
+                const filteredArray=jsob_data.filter((news,index)=>{return this.current_page*10<=index && index<this.current_page*10+10});
+                document.getElementsByTagName("main")[0].insertAdjacentHTML("afterbegin",
+                    filteredArray.map(newNews=>{
+                        const articleObj = new RecentNewsItem(newNews);
+                        return articleObj.Render();
+                    })
+                    .reduce((prev, cur)=>prev+cur,"")
+                );
+            })
+        })
+        .catch(err => { console.log(err); });
+    }
+    render(){
+    }
+    pages(data_len){
+        data_len=Math.ceil(data_len/10);
+        data_len*=10;
+        console.log(data_len);
+        let ret=`<a href="#" onclick="recentNews.download()"> Previous </a>`
+            let start=parseInt(this.current_page/3)*3;
+            console.log(data_len);
+            console.log(start);
+            data_len=parseInt((data_len-(start+1)*10)/10);
+            if(3<data_len){
+                data_len=3;
+            }
+            for (let i=start;i<=start+data_len;i++) {
+                ret+=`<a href="#" onclick="recentNews.download()"> ${i+1} </a>`;
+            }
+        
+            
+        
+        ret+=`<a href="#" onclick=""> Next </a>`;
+        document.querySelector(".link").innerHTML+=ret;
+    }   
+     */
