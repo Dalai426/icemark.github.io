0eb943fba1ff05e4f0bcdd01f5c7fa1cf3794c52
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-11-25 08:44:29 +0800

    Update medee_medeelel.js

--- modules/medee_medeelel.js
+++ modules/medee_medeelel.js
@@ -1,5 +1,4 @@
 class RecentNewsItem { 
-    //constructor
     constructor(news) {
         this.src = news.src;
         this.mmin = news.min;
@@ -9,7 +8,6 @@ class RecentNewsItem {
         this.content=news.content;
         this.date=news.date;
     }
-    //build and return html
     Render() {
         return ` <section class="news"><a href="${this.href}">
         <picture>
@@ -43,6 +41,7 @@ export default class RecentNews{
             response.json()
             .then(jsob_data=>{
                 document.getElementsByTagName("main")[0].innerHTML=``;
+                this.pages(jsob_data.length);
                 const filteredArray=jsob_data.filter((news,index)=>{return this.current_page*10<=index && index<this.current_page*10+10});
                 document.getElementsByTagName("main")[0].insertAdjacentHTML("afterbegin",
                     filteredArray.map(newNews=>{
@@ -74,6 +73,11 @@ export default class RecentNews{
         let contain=document.querySelector(".link");
         contain.innerHTML=``;
         contain.innerHTML+=ret;
+        document.getElementById("nex").addEventListener("click",()=>(this.page_p(1)));
+        document.getElementById("prv").addEventListener("click",()=>(this.page_p(-1)));
+        for (let i=start;i<=start+data_len;i++) {
+            document.getElementsByClassName("page")[i-start].addEventListener("click",()=>{this.change_page(i)});
+        }
     } 
     page_p(page){
         if(this.current_page==0 && page==-1){
@@ -91,89 +95,3 @@ export default class RecentNews{
     }
 }
 
-
-/*download(){
-        this.jsobs=fetch(`${this.jsonUrl}`)
-        .then(response=>{
-            response.json()
-            .then(
-                jsob_data=>{
-                            console.log(jsob_data);
-                            jsob_data.map(newNews=>{
-                                const articleObj = new RecentNewsItem(newNews);
-                                this.recentNewsList.push(articleObj);});
-                                console.log(this.recentNewsList.length);
-                }
-                
-            )
-        })
-        .catch(err => { console.log(err) });
-    }
-    render(){
-        console.log(this.recentNewsList.length);
- 
-            document.getElementsByTagName("main")[0].insertAdjacentHTML("afterbegin",
-                this.recentNewsList.map(newNews=>{
-                    const articleObj = new RecentNewsItem(newNews);
-                    return articleObj.Render();
-                })
-                .reduce((pre, cur) => pre+cur, "")
-            );
-        
- 
-    }
-*/
-
-/*
-constructor(jsonUrl) {
-        this.recentNewsList = [];
-        this.jsonUrl = jsonUrl;
-        this.jsobs;
-        this.current_page=4;
-    }
-    download(){
-        this.jsobs=fetch(`${this.jsonUrl}`)
-        .then(response=>{
-            response.json()
-            .then(jsob_data=>{
-                this.len=jsob_data.length;
-                const filteredArray=jsob_data.filter((news,index)=>{return this.current_page*10<=index && index<this.current_page*10+10});
-                document.getElementsByTagName("main")[0].insertAdjacentHTML("afterbegin",
-                    filteredArray.map(newNews=>{
-                        const articleObj = new RecentNewsItem(newNews);
-                        return articleObj.Render();
-                    })
-                    .reduce((prev, cur)=>prev+cur,"")
-                );
-            })
-        })
-        .catch(err => { console.log(err); });
-    }
-
-    render(){
-    }
-
-
-    pages(data_len){
-        data_len=Math.ceil(data_len/10);
-        data_len*=10;
-        console.log(data_len);
-        let ret=`<a href="#" onclick="recentNews.download()"> Previous </a>`
-
-            let start=parseInt(this.current_page/3)*3;
-            console.log(data_len);
-            console.log(start);
-            data_len=parseInt((data_len-(start+1)*10)/10);
-            if(3<data_len){
-                data_len=3;
-            }
-            for (let i=start;i<=start+data_len;i++) {
-                ret+=`<a href="#" onclick="recentNews.download()"> ${i+1} </a>`;
-            }
-        
-            
-        
-        ret+=`<a href="#" onclick=""> Next </a>`;
-        document.querySelector(".link").innerHTML+=ret;
-    }   
-     */
