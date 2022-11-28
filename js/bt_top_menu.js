de1debd169ae48197913d633fd3f5b0de95e8336
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-11-28 13:04:15 +0800

    bt_top_sub_menu

--- js/bt_top_menu.js
+++ js/bt_top_menu.js
@@ -2,7 +2,8 @@ let mql = window.matchMedia('(min-width: 691px)');
 let scroll_pos;
 window.onscroll=()=>{scroll()};
 function scroll() {
-let elm=document.querySelector(".bvteegdehvvnii_turul");
+    resize_top_menu();
+    let elm=document.querySelector(".bvteegdehvvnii_turul");
     if (document.documentElement.scrollTop > scroll_pos && mql.matches) {
         elm.style.display="block";   
     } else {
