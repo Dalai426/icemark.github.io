d8bb7fe7a935b16fa4adc1907700da3e7522a61d
Tsogbadrakh <Gtsog321@gmail.com>
2022-12-03 22:53:32 +0800

    phonelist in header footer

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,4 +1,4 @@
-import {phonelist} from "../modules/phone_list.js";
+
 class navbar extends HTMLElement {
     connectedCallback() {
       this.parentNode.innerHTML = `
@@ -118,13 +118,28 @@ class navbar extends HTMLElement {
   customElements.define("icemark-open",openlist);
   
 
+  class phonelist{
+    constructor(width,list,open) {
+        this.width=window.matchMedia('(min-width:'+width+'px)');
+        this.event_el=document.getElementById(list);
+        this.open_el=document.querySelector("."+open);
+        this.state="none";
+        this.open_name=open;
+    }
+    mediaquery_event(){
+        window.addEventListener("resize",(e)=>{
+            if(this.width.matches){
+                this.open_el.classList.remove("active");
+            }
+        });
+    }
+    event(){
+        this.event_el.addEventListener("click",()=>{
+            this.open_el.classList.toggle("active");
+        });
+    }
 
-
- 
-
-
-
-
+}
 
 let scrollbar=`<style>
     ::-webkit-scrollbar {
