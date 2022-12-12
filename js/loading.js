5172f192e77455f0dc1429db6a68fb962ba7adb0
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2022-12-12 21:16:52 +0800

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,4 @@
+//content-uud unshij baih vyed 500 hugacaand giff zurag bvhii div element haruulna
 window.addEventListener("DOMContentLoaded",()=>{
     const loader=document.getElementById("loader");
     setTimeout(()=>{
