abf48d475c5d9e705cd3b5b431cb22fcef009c98
Tsogbadrakh-G <83754122+Tsogbadrakh-G@users.noreply.github.com>
2022-12-13 20:05:16 +0800

    Update index_slider.js

--- js/index_slider.js
+++ js/index_slider.js
@@ -6,7 +6,7 @@ class RecentSliderItems{
         this.min_src=min_src;
     }
     render_a(){
-        return `<div class="slide" style="background-image:url(${this.src});"></div>  `;
+        return `<div class="slide" style="background-image:url(${this.src}); z-index: ${this.zIndex}; "></div>  `;
     }
   
   }
@@ -20,12 +20,12 @@ function download(jsonUrl){
     
             for (const key of jsob_data){
         
-                let articleObj = new RecentSliderItems(key.src,key.max_src,key.min_src);
+                let articleObj = new RecentSliderItems(key.src,key.max_src,key.min_src,key.zIndex);
                 retVal += articleObj.render_a();
             }
         
             document.getElementsByClassName("slider")[0].innerHTML=retVal;
-            sliding()
+            sliding();
             })
         })
     
@@ -36,17 +36,22 @@ window.onload=()=>download("./json/index_slider.json");
 
 function sliding(){
     let slideIndex = 0;
-    showSlides();
+    next();
+
+      function next(){
+        let slides = document.getElementsByClassName("slide");
+        for (i = 0; i < slides.length; i++) {
+             slides[i].style.zIndex = "0";
+            
+          }
+
+          slideIndex++;
+          if (slideIndex > slides.length) {slideIndex = 1}
+          slides[slideIndex-1].style.zIndex = "30";
+          setTimeout(next, 2000);
     
-    function showSlides() {
-      let i;
-      let slides = document.getElementsByClassName("slide");
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}
-      slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides, 2000);
-    }
-}
+     
+    
+              }
+          }
+    
